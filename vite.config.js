c77be37ef5ef015b734c7d96aabd5b4118dd4b46
md-nafis-sadik <sadikmdnafis@gmail.com>
2024-08-17 21:04:20 +0600

    Updated vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   base: '/data-website-react/', // Make sure this matches your repository name
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html', // Ensure this path is correct
+    },
+  },
 });
